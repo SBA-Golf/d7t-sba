05d6579ea7204859b0e73b26ca6a3cb3f3672d28
Iñaki García Blasco <txorua@txorua.com>
2015-07-13 19:27:47 +0200

    Abro enlace de patrocinador en ventana nueva.

--- templates/views/views-view-fields--patrocinios--block-1.tpl.php
+++ templates/views/views-view-fields--patrocinios--block-1.tpl.php
@@ -1,5 +1,5 @@
 <div class="item">
-<a href="<?php print render($row->field_field_patrocinador_web[0]['rendered']); ?>">
+<a href="<?php print render($row->field_field_patrocinador_web[0]['rendered']); ?>" target="_blank">
 <?php print render($row->field_field_patrocinador_logo[0]['rendered']); ?>
 </a>
 </div>
