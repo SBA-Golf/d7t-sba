e0e5e7d97a7fe82d8e4371cf3ca24f1f2599508d
Iñaki García Blasco <txorua@txorua.com>
2015-07-13 19:07:20 +0200

    Añado enlaces a patrocinadores

--- templates/views/views-view-fields--patrocinios--block-1.tpl.php
+++ templates/views/views-view-fields--patrocinios--block-1.tpl.php
@@ -1,3 +1,5 @@
 <div class="item">
+<a href="<?php print render($row->field_field_patrocinador_web[0]['rendered']); ?>">
 <?php print render($row->field_field_patrocinador_logo[0]['rendered']); ?>
+</a>
 </div>
