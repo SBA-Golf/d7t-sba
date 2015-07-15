6b4dd946cc15156766342e9344e62afa14989694
Iñaki García Blasco <txorua@txorua.com>
2015-07-15 14:01:04 +0200

    Añado enlaces a patrocinado en vista teaser

--- templates/node--patrocinador.tpl.php
+++ templates/node--patrocinador.tpl.php
@@ -5,6 +5,10 @@
 ?>
 <?php if ($teaser): ?>
 <article class="node-patrocinador teaser">
-<?php print render($content); ?>
+<?php if ($variables['field_patrocinador_web']): ?>
+  <a href="<?php print $variables['field_patrocinador_web']['und'][0]['url']; ?>" target="_blank"><?php print render($content); ?></a>
+<?php else: ?>
+  <?php print render($content); ?>
+<?php endif; ?>
 </article>
 <?php endif; ?>
