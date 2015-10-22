bfb2ca3e18c9af0f63e037943d65b230f1ef786e
Iñaki García Blasco <txorua@txorua.com>
2015-10-22 16:49:56 +0200

    Cambio estilo de bloque upcomming en portada

---
+++ templates/views/views-view-list--calendario-de-torneos--block-3.tpl.php
@@ -0,0 +1,13 @@
+<?php print $wrapper_prefix; ?>
+  <?php if (!empty($title)) : ?>
+    <h3><?php print $title; ?></h3>
+  <?php endif; ?>
+  <?php print $list_type_prefix; ?>
+  <div class="row">
+    <?php foreach ($rows as $id => $row): ?>
+      <li class="<?php print $classes_array[$id]; ?>"><?php print $row; ?></li>
+    <?php endforeach; ?>
+  </div>
+  <?php print $list_type_suffix; ?>
+<hr/>
+<?php print $wrapper_suffix; ?>
