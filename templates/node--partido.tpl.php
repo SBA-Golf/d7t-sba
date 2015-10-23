75926ace4b4b602d1f6fe79ced8bcbf42100f545
Iñaki García Blasco <txorua@txorua.com>
2015-10-23 13:58:32 +0200

    Añado información a los partidos

--- templates/node--partido.tpl.php
+++ templates/node--partido.tpl.php
@@ -21,14 +21,26 @@
 
 <?php if ($view_mode == 'full'): ?>
 
+
 <article class="node-partido full">
-  <div class="content">
-  <?php hide($content['field_partido_patrocinadores']); ?>
-  <?php hide($content['field_partido_cronica']); ?>
   <?php hide($content['field_partido_clasificaciones']); ?>
+  <?php hide($content['field_partido_cronica']); ?>
+  <?php hide($content['field_partido_menu_comida']); ?>
+  <?php hide($content['field_partido_titulo_charla']); ?>
+  <?php hide($content['field_partido_ponente_charla']); ?>
+  <?php hide($content['field_partido_resumen_charla']); ?>
+  <?php hide($content['field_partido_adjuntos_charla']); ?>
+  <?php hide($content['field_partido_patrocinadores']); ?>
   <?php hide($content['galeria_de_fotos_de_torneo_entity_view_1']); ?>
+
+  <section id="avisos" class="bg-info">
+  <?php print render($content['field_partido_ultima_hora']); ?>
+  </section>
+
+  <main class="content">
   <?php print render($content); ?>
-  </div>
+  </main>
+
   <?php if (user_is_logged_in()): ?>
   <section id="documentos">
   <?php if (!empty($content['field_partido_cronica']['#items']) || !empty($content['field_partido_clasificaciones']['#items'])): ?>
@@ -38,9 +50,24 @@
   <?php endif; ?>
   </section>
   <?php endif; ?>
+
+  <section id="comida">
+    <h2><?php print t('Comida'); ?></h2>
+    <?php print render($content['field_partido_menu_comida']); ?>
+  </section>
+
+  <section id="charla">
+    <h2><?php print t('Charla'); ?></h2>
+    <?php print render($content['field_partido_titulo_charla']); ?>
+    <?php print render($content['field_partido_ponente_charla']); ?>
+    <?php print render($content['field_partido_resumen_charla']); ?>
+    <?php print render($content['field_partido_adjuntos_charla']); ?>
+  </section>
+
   <section id="galeria">
     <?php print render($content['galeria_de_fotos_de_torneo_entity_view_1']); ?>
   </section>
+
   <section id="patrocinios">
     <h2><?php print t('Patrocinadores'); ?></h2>
     <?php print render($content['field_partido_patrocinadores']); ?>
