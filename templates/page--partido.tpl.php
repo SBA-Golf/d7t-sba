fdc52abb417cabae2cbfcf9596749923b6d3624d
Iñaki García Blasco <txorua@txorua.com>
2016-05-18 10:47:55 +0200

    Alíneo los logos al pie según indicaciones de Carlos

--- templates/page--partido.tpl.php
+++ templates/page--partido.tpl.php
@@ -94,9 +94,11 @@
   <section id="footer-section-2">
     <div class="container">
       <div class="row">
-        <div class="col-xs-12 col-md-4 pull-right">
+        <div class="col-xs-12 col-md-4">
           <img src="<?php print $logo; ?>" class="img-responsive" typeof="foaf:Image">
           <br>
+        </div>
+        <div class="col-xs-12 col-md-4 pull-right">
           <a href="http://golfbasozabal.com/" target="_blank"><img src="/<?php print drupal_get_path('theme','sba') . '/a/i/basozabal.png'; ?>" class="img-responsive center-block" typeof="foaf:Image"><p class="text-center">www.golfbasozabal.com</p></a>
           <?php print render($page['legal']); ?>
         </div>
