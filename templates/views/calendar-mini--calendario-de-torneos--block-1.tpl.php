206f5839ce33f30e4d57db2da512bf094d7829e9
Iñaki García Blasco <inaki@txorua.com>
2018-04-28 12:50:23 +0200

    Función split() deprecated in php 7. La sustituyo por explode()

--- templates/views/calendar-mini--calendario-de-torneos--block-1.tpl.php
+++ templates/views/calendar-mini--calendario-de-torneos--block-1.tpl.php
@@ -2,7 +2,7 @@
 /**
   * @file
   * Template to display a view as a mini calendar month.
-  * 
+  *
   * @see template_preprocess_calendar_mini.
   *
   * $day_names: An array of the day of week names for the table header.
@@ -10,12 +10,12 @@
   * $view: The view.
   * $min_date_formatted: The minimum date for this calendar in the format YYYY-MM-DD HH:MM:SS.
   * $max_date_formatted: The maximum date for this calendar in the format YYYY-MM-DD HH:MM:SS.
-  * 
+  *
   * $show_title: If the title should be displayed. Normally false since the title is incorporated
   *   into the navigation, but sometimes needed, like in the year view of mini calendars.
-  * 
+  *
   */
-//dsm('Display: '. $display_type .': '. $min_date_formatted .' to '. 
+//dsm('Display: '. $display_type .': '. $min_date_formatted .' to '.
 //$max_date_formatted);dsm($day_names);
 $params = array(
   'view' => $view,
@@ -44,8 +44,8 @@ $colores = array();
         <?php print theme('date_nav_title', $params) ?>
       </div>
     </div>
-  </div> 
-  <?php endif; ?> 
+  </div>
+  <?php endif; ?>
   <table class="mini">
     <thead>
       <tr>
@@ -61,7 +61,7 @@ $colores = array();
       <tr>
         <?php foreach ($row as $cell): ?>
           <td id="<?php print $cell['id']; ?>" class="<?php print $cell['class']; ?>">
-            <?php if (in_array('has-events',split(' ',$cell['class']))): ?>
+            <?php if (in_array('has-events',explode(' ',$cell['class']))): ?>
             <?php //dsm($cell['data']); ?>
             <?php
               $cal_dia = explode('-',$cell['id'])[3];
